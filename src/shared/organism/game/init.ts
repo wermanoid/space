eb9e59059f4d365c9447466ece3af0825168d324
wermanoid <wermanoid@gmail.com>
2019-08-28 15:53:58 +0200

    chore(nothing): just case study

--- src/shared/organism/game/init.ts
+++ src/shared/organism/game/init.ts
@@ -1,48 +1,26 @@
 import * as THREE from 'three';
 import { OrbitControls } from './orbit-controls';
 
-import { GraphicsSystem } from './systems/graphics';
-import { Mesh } from 'three';
-
-const loadGraphics = (graphics: GraphicsSystem) => {
-  graphics.addGeomerty(
-    'block',
-    new THREE.BoxGeometry(10, 10, 10).translate(0, 0, 5)
-  );
-  graphics.addGeomerty(
-    'plate',
-    new THREE.BoxGeometry(10, 10, 1).translate(0, 0, 0.5)
-  );
-
-  graphics.addMaterial(
-    'stone',
-    new THREE.MeshLambertMaterial({
-      color: 0x333333,
-    })
-  );
-  graphics.addMaterial(
-    'quartz',
-    new THREE.MeshLambertMaterial({
-      color: 0x999999,
-    })
-  );
-
-  return graphics;
-};
+import { loadGraphics } from './systems/graphics';
 
 export const init = (canvas: HTMLCanvasElement) => {
-  const graphics = loadGraphics(new GraphicsSystem());
+  const graphics = loadGraphics();
 
   const scene = new THREE.Scene();
+  scene.background = new THREE.Color('#777');
   const camera = new THREE.PerspectiveCamera(
-    75,
+    35,
     canvas.width / canvas.height,
     0.1,
-    1000
+    500
   );
 
   const controls = new OrbitControls(camera, canvas);
-  const renderer = new THREE.WebGLRenderer({ canvas });
+  const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+  renderer.gammaFactor = 2.2;
+  renderer.gammaOutput = true;
+  renderer.physicallyCorrectLights = true;
+
   // renderer.setSize(canvas.width, canvas.height);
   const geometry = new THREE.PlaneGeometry(100, 100);
   const material = new THREE.MeshBasicMaterial({
@@ -52,12 +30,20 @@ export const init = (canvas: HTMLCanvasElement) => {
     opacity: 0.2,
   });
   const layer = new THREE.Mesh(geometry, material);
-  layer.rotation.x = -Math.PI / 2;
+  layer.matrixAutoUpdate = false;
+
+  // layer.rotation.x = -Math.PI / 2;
 
   // some stuff...
 
+  const edges = new THREE.EdgesGeometry(graphics.geometries['block']);
+  const line = new THREE.LineSegments(
+    edges,
+    new THREE.LineBasicMaterial({ color: 0xffffff })
+  );
+
   const items: THREE.Mesh[] = [];
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < 100; i++) {
     const item = new THREE.Mesh(
       graphics.geometries[i % 2 === 0 ? 'block' : 'plate'],
       graphics.materials[i % 2 === 0 ? 'stone' : 'quartz']
@@ -68,13 +54,15 @@ export const init = (canvas: HTMLCanvasElement) => {
       0
     );
     items[i] = item;
+    setTimeout(
+      xx => {
+        layer.add(xx);
+      },
+      i * 100,
+      item
+    );
   }
 
-  // const edges = new THREE.EdgesGeometry(graphics.geometries['block']);
-  // const line = new THREE.LineSegments(
-  //   edges,
-  //   new THREE.LineBasicMaterial({ color: 0xffffff })
-  // );
   // lights
   const ambientLight = new THREE.AmbientLight(0xcccccc, 0.4);
   scene.add(ambientLight);
@@ -86,26 +74,16 @@ export const init = (canvas: HTMLCanvasElement) => {
   const sceneGrid = new THREE.GridHelper(140, 14);
   const grid = new THREE.GridHelper(100, 10, 0xff0000, 0xff0000);
   scene.add(sceneGrid);
+  sceneGrid.rotation.x = Math.PI / 2;
+  grid.rotation.x = Math.PI / 2;
 
   layer.add(grid);
-  // cube.add(cube2);
-  // cube2.add(line);
-  layer.add(...items);
-
-  setTimeout(() => {
-    items[4].material = graphics.materials.quartz;
-  }, 4000);
-
-  setTimeout(() => {
-    items[4].material.dispose();
-    items[4].geometry.dispose();
-    layer.remove(items[4]);
-    items[4] = new Mesh(graphics.geometries.block, graphics.materials.stone);
-    layer.add(items[4]);
-  }, 5000);
+  // layer.add(...items);
 
   scene.add(layer);
-  scene.add(new THREE.AxesHelper(20));
+
+  const axes = new THREE.AxesHelper(10);
+  scene.add(axes);
 
   // const l2 = cube.clone();
   // scene.add(l2);
@@ -117,19 +95,24 @@ export const init = (canvas: HTMLCanvasElement) => {
   // l3.position.y = -25;
 
   // grid.position.set(0, 0, 0);
-  grid.rotation.x = Math.PI / 2;
 
-  // cube.position.set(0, 0, 0);
-  // cube2.position.set(0, 0, 0);
+  // grid.rotation.x = Math.PI / 2;
 
-  camera.position.x = 5;
-  camera.rotation.x = (-50 * Math.PI) / 180;
-  camera.rotation.y = (25 * Math.PI) / 180;
-  camera.position.z = 50;
-  camera.position.y = 100;
+  // camera.position.x = 5;
+  // camera.rotation.x = (-50 * Math.PI) / 180;
+  // camera.rotation.y = (25 * Math.PI) / 180;
+  // camera.position.z = 50;
+  // camera.position.y = 100;
+
+  camera.position.set(0, -50, 150);
+  camera.lookAt(0, 0, 0);
+  camera.rotation.z = (15 * Math.PI) / 180;
+
+  // camera.rotation.x = (-90 * Math.PI) / 180;
 
   // scene.rotation.x = (25 * Math.PI) / 180;
   controls.update();
+
   const animate = function(passed) {
     requestAnimationFrame(animate);
 
@@ -140,6 +123,17 @@ export const init = (canvas: HTMLCanvasElement) => {
     // camera.position.y -= 0.1;
     controls.update();
     renderer.render(scene, camera);
+  };
+
+  window.stats = () => {
+    console.log('renderer.info.render.frame', renderer.info.render.frame);
+    console.log('renderer.info.programs', renderer.info.programs);
+    console.log('renderer.info.memory.textures', renderer.info.memory.textures);
+    console.log(
+      'renderer.info.memory.geometries',
+      renderer.info.memory.geometries
+    );
+    console.log('renderer.info.render.calls', renderer.info.render.calls);
   };
 
   animate(0);
--- src/shared/organism/game/systems/graphics.ts
+++ src/shared/organism/game/systems/graphics.ts
@@ -1,13 +1,46 @@
-import * as three from 'three';
+import {
+  BoxBufferGeometry,
+  BufferGeometry,
+  Material,
+  MeshLambertMaterial,
+} from 'three';
 
 export class GraphicsSystem {
-  public geometries: Record<string, three.Geometry> = {};
-  public materials: Record<string, three.Material> = {};
+  public geometries: Record<string, BufferGeometry> = {};
+  public materials: Record<string, Material> = {};
 
-  public addGeomerty(name: string, geometry: three.Geometry) {
+  public addGeomerty(name: string, geometry: BufferGeometry) {
     this.geometries[name] = geometry;
   }
-  public addMaterial(name: string, material: three.Material) {
+  public addMaterial(name: string, material: Material & { color?: any }) {
     this.materials[name] = material;
+    material.color && material.color.convertSRGBToLinear();
   }
 }
+
+export const loadGraphics = () => {
+  const graphics = new GraphicsSystem();
+  graphics.addGeomerty(
+    'block',
+    new BoxBufferGeometry(10, 10, 10).translate(0, 0, 5)
+  );
+  graphics.addGeomerty(
+    'plate',
+    new BoxBufferGeometry(10, 10, 1).translate(0, 0, 0.5)
+  );
+
+  graphics.addMaterial(
+    'stone',
+    new MeshLambertMaterial({
+      color: 0x333333,
+    })
+  );
+  graphics.addMaterial(
+    'quartz',
+    new MeshLambertMaterial({
+      color: 0x999999,
+    })
+  );
+
+  return graphics;
+};
